e15cad1b5cb1ff8f2611f264cfad0bf178ef8148
proph3tsix <prsnl.isaiah.williams@gmail.com>
2024-01-15 13:17:23 -0600

    saving changes

---
+++ LTAF_scanner.py
@@ -0,0 +1,11 @@
+# Checks a specified directory for a specified file
+# Returns the path to the specified file, as well as the file size
+# Option to return the path to the specified file IF the file exceeds a specified size
+# Option to return the path to the specified file IF the number of rows in the file are greater than 1 (is populated)
+# Option to return the 
+
+
+
+# specify directory
+top_dir = input('Specify the top directory to search:  ')
+print(top_dir)
--- guibox.py
+++ guibox.py
@@ -1,8 +1,4 @@
-# Checks a specified directory for a specified file
-# Returns the path to the specified file, as well as the file size
-# Option to return the path to the specified file IF the file exceeds a specified size
-# Option to return the path to the specified file IF the number of rows in the file are greater than 1 (is populated)
-# Option to return the 
+
 
 # Python Helper Code Snippet #5
 import tkinter as tk
